5f144c373d51a06f00e80027326af18012074e3f
Laurence Martin <floz@credersi.com>
2022-08-11 14:11:28 +0100

    Final implementation of the steps for the first learning outcome

--- features/support/steps.js
+++ features/support/steps.js
@@ -7,22 +7,57 @@
  * - Arrow functions not supported by cucumber when accessing the 'world' class
  *   SEE: github.com/cucumber/cucumber-js/blob/main/docs/support_files/world.md
  * 
- * Given:
- *  a fibonacci sequence initialized to 8
- *  a fibonacci sequence initialized to 4181
- *  a fibonacci sequence initialized to 2
- * "a fibonacci sequence initialized to {int}"
- *  
  ******************************************************************************
 */
 
-const assert = require("assert");
-const {Given, When, Then} = require("@cucumber/cucumber");
+const assert = require('assert');
+const {Given, When, Then} = require('@cucumber/cucumber');
 
-Given("a fibonacci sequence  initialized to {int}", function (value) {
+// ----------------------------------------------------------------------------
+// Steps for the first & second learning outcomes
+// ----------------------------------------------------------------------------
+
+Given("a fibonacci sequence initialized to {int}", function(value) {
     this.fibonacci.init(value);
 });
 
 When("a fibonacci sequence is started", function() {
     // Nothing to do, as done by the custom world
+});
+
+When("a fibonacci sequence is initialized to {int}", function(value) {
+    try {
+        this.fibonacci.init(value);
+    } catch {
+        this.itThrew();
+    }
+});
+
+When("the sequence is skipped {int} time(s)", function(value) {
+    this.fibonacci.skip(value);
+});
+
+Then("the next {word} should be {}", function(action, value) {
+    switch(action) {
+        case "number":
+            assert(this.fibonacci.next() == parseInt(value));
+            break;
+        
+        case "sequence":
+            value.split(",").forEach((expected) => {
+                assert(this.fibonacci.next() == parseInt(expected.trim()));
+            });
+            break;
+        
+        default:
+            throw new Error(`Unsupported action ${action}`);
+    }
+});
+
+Then("it should throw an exception", function() {
+    assert(this.hasThrown());
+});
+
+Then("the state should be {string}", function(value) {
+    assert(this.fibonacci.state() == value);
 });
