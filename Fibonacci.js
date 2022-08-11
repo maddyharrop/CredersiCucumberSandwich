66ef73b2e319c8ff50223a3827f91204fead93eb
Laurence Martin <floz@credersi.com>
2022-08-11 14:13:03 +0100

    Final implementation of code for the second learning outcome

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,34 +1,38 @@
-/**
- * 
- * next returns:
- * The integer of the next Fibonacci Number in the sequence
- * 
- * init:
- * Takes a Fibonacci seed number to initialize from
- * 
- * skip:
- * Takes a number of how many Fibonacci Numbers to skip
- * 
- * state returns:
- * "The current number is <Current Fibonacci Number>"
- * 
- */
-
 class Fibonacci {
+    constructor() {
+        this.previous = 0;
+        this.current = 1; 
+    }
+
     next() {
-        throw new Error("The next function is not yet supported!");
+        const next = this.previous + this.current;
+        
+        this.previous = this.current;
+        this.current = next;
+
+        return next;
     }
 
     init(seed) {
-        throw new Error("The init function is not yet supported!");
+        while (this.current < seed) {
+            this.next();
+        }
+
+        if (this.current != seed) {
+            throw new Error('Invalid Fibonacci seed number!');
+        }
     }
 
     skip(forward) {
-        throw new Error("The skip function is not yet supported!");
+        for (let i = 0; i < (forward - 1); i++) {
+            this.next();
+        }
+
+        return this.next();
     }
 
     state() {
-        throw new Error("The state function is not yet supported!");
+        return `The current number is ${this.current}`;
     }
 }
 
